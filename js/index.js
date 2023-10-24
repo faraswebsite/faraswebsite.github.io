536232b940ae56c2730823d49e79f4ada79c289a
sakanusiw <saka.interisti@gmail.com>
2023-10-24 17:34:37 +0700

    changed all

--- js/index.js
+++
@@ -1,5 +0,0 @@
-setTimeout(function () {
-    var splashScreen = document.querySelector("#splash");
-    splashScreen.parentNode.removeChild(splashScreen);
-    window.location.href = "/home.html";
-  }, 3000);
